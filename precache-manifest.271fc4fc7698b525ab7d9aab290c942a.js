1081fb1d108d9e273bc56a609e03194c1a4322c6
Deepak <deepak25mb@gmail.com>
2019-06-15 21:43:48 +0530

    Updates

---
+++ precache-manifest.271fc4fc7698b525ab7d9aab290c942a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041b34791da421e03e5f0a70cf4a40ec",
+    "url": "/12-foodmenu/index.html"
+  },
+  {
+    "revision": "73d46738903d76946008",
+    "url": "/12-foodmenu/static/css/main.37e60f93.chunk.css"
+  },
+  {
+    "revision": "4eb1f4537399a0f880a0",
+    "url": "/12-foodmenu/static/js/2.5d0193f5.chunk.js"
+  },
+  {
+    "revision": "73d46738903d76946008",
+    "url": "/12-foodmenu/static/js/main.c010b3a0.chunk.js"
+  },
+  {
+    "revision": "a30f2ddc579af6593a2a",
+    "url": "/12-foodmenu/static/js/runtime~main.8c68a625.js"
+  }
+]);
